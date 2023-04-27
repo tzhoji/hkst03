ea863e83091a35a2e212d8fe7482f7d8821c899d
tzhoji <57669967+tzhoji@users.noreply.github.com>
2023-04-27 14:31:05 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import { Routes, Route } from 'react-router-dom';
-
+import { Fragment } from 'react';
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
-import React from 'react';
-import Chatbot from './chat';
-
-
+import ContactForm from './routes/contact/contact.component';
+import Chatbot from './chat'
 
 const App = () => {
   return (
-    <React.Fragment>
-      <Routes>
-        <Route path='/' element={<Navigation />}>
-          <Route index element={<Home />} />
-          <Route path='shop/*' element={<Shop />} />
-          <Route path='auth' element={<Authentication />} />
-          <Route path='checkout' element={<Checkout />} />
-        </Route>
-      </Routes>
-      <Chatbot />
-    </React.Fragment>
+    <Fragment>
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop/*' element={<Shop />} />
+        <Route path='auth' element={<Authentication />} />
+        <Route path='checkout' element={<Checkout />} />
+        <Route path='contact' Component={ContactForm} />
+      </Route>
+    </Routes>
+    <Chatbot />
+    </Fragment>
   );
 };
 
